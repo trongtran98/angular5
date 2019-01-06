5bd0761c528ebb334b34b6626e02d7fc9f451941
trongtq98 <trongtqk68@gmail.com>
2019-01-06 21:48:03 +0700

    *ngFor and *ngIf

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,19 @@
+import {Movie} from '../models/movie';
+
+export let fakeMovie: Movie[] = [
+  {
+    id: 1,
+    name: 'Star wars',
+    releaseYear: 2000
+  },
+  {
+    id: 2,
+    name: 'Aquaman',
+    releaseYear: 2019
+  },
+  {
+    id: 3,
+    name: 'Xmen',
+    releaseYear: 2001
+  }
+];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from '../../models/movie';
+import {fakeMovie} from '../fake-movies';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  movie: Movie = {
+    id: 1,
+    name: 'Star Wars',
+    releaseYear: 1977
+  };
+  movies = fakeMovie;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  // Action when select a Movie in list item
+  selectedMovie: Movie;
+
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+    console.log(`selectedMovie = ${JSON.stringify(this.selectedMovie)}`);
+  }
 }
